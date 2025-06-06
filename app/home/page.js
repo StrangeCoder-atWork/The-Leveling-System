80d5561c89ae1fa330e7d051f1113851d3b93b02
Noname <155514756+UnknownName007@users.noreply.github.com>
2025-06-06 16:56:03 +0530

    changess

--- app/home/page.js
+++ app/home/page.js
@@ -1,4 +1,4 @@
-
+"use client"
 import React from 'react'
 import {getServerSession} from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]/route"
@@ -12,12 +12,7 @@ const Home_design = dynamic(() => import("../components/home.js"), { ssr: false 
 export default function Page(props) {
   return <Home_design {...props} />;
 }
-const Home = async () => {
-    
-  // const session = await getServerSession(authOptions);
-  // if(!session){
-  //   redirect("/login");
-  // }
+const Home =  () => {
    
   return (
     <>
