810d75e57b2ffbceb7dcc6dfca0d0b8787db9917
Noname <155514756+UnknownName007@users.noreply.github.com>
2025-06-06 17:04:08 +0530

    chg

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,5 @@
 "use client"
 import React from 'react'
-import {getServerSession} from "next-auth/next"
-import { authOptions } from "../api/auth/[...nextauth]/route"
-import {redirect} from "next/navigation"
-
-import Cards from '../components/Cards'
 import dynamic from "next/dynamic";
 
 const Home_design = dynamic(() => import("../components/home.js"), { ssr: false });
